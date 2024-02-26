fe589a9aff4148151b9bf4b5ca897aa3d7ff96ed
pytholabs_bot1 <pytholabs@gmail.com>
2024-02-26 16:11:52 +0530

    v 7

--- example/urls.py
+++ example/urls.py
@@ -1,9 +1,21 @@
-# example/urls.py
 from django.urls import path
 
-from .views import home_view
+from . import views
 
 urlpatterns = [
-    # ... other patterns...
-    path('', home_view, name='home'),
+    path('', views.index, name='index'),
+    path("project/<str:project>",views.property,name="property"),
+    path("test",views.PropTest,name="test"),
+
+    path("sndwa/<str:index>", views.sendwa, name="mark"),
+    # path("loadcsv", view=views.loadcsv),
+    path("mcll", view=views.mark_call),
+    path("privacy", view=views.privacy),
+    path("lepost", view=views.lepost),
+    path("dash", view=views.dash),
+    path("fetch_leads", views.fresh_leads, name="ff"),
+    path("tour", views.tour, name="tour"),
+    path("api/<str:query>", view=views.json_api),
+    path("list/<str:mod_id>", view=views.LeadListView),
+    path("callredirect/<str:num>", view= views.call_redirect),
 ]
